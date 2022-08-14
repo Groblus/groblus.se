a8db3ade5950d2d04b5b13be2246217ccd5242cb
Dan Strokirk <dan.strokirk@gmail.com>
2022-08-14 22:17:22 +0200

    Lägg till config för eleventy

---
+++ .eleventy.js
@@ -0,0 +1,6 @@
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
+  eleventyConfig.addPassthroughCopy("downloads");
+};
